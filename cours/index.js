68a74b04289546a38e6ce7e591c93c78df4a14e1
Mathieu Etourneaud <mathieu.etourneaud@lapiscine.pro>
2023-10-19 17:30:41 +0200

    Array and Objects

--- cours/index.js
+++ cours/index.js
@@ -89,8 +89,120 @@ const is30YearsOld = myAge == 30
 // }
 
 
-if(is30YearsOld) {
-    console.log(`Mathieu a 30 ans`)
+// if(is30YearsOld) {
+//     console.log(`Mathieu a 30 ans`)
+// } else {
+//     console.log(`Mathieu n'a pas 30 ans`)
+// }
+
+
+
+const bigNb1 = 100
+const bigNb2 = 400
+
+//écrire la condition pour afficher le plus grand nombre
+
+// const resultComparison = bigNb1 > bigNb2
+
+// if(resultComparison) {
+//     console.log(bigNb1)
+// } else {
+//     console.log(bigNb2)
+// }
+
+
+// if(bigNb1 > bigNb2) {
+//     console.log(bigNb1)
+// } else if(bigNb1 < bigNb2) {
+//     console.log(bigNb2)
+// } else if(bigNb1 == bigNb2) {
+//     console.log(bigNb1, bigNb2)
+// }
+
+
+const firstname = "Olivier"
+// const firstname2 = "Hamid"
+// const firstname3 = "Pauline"
+
+if (firstname == "Hamid") {
+    // console.log('bonjour mon cher ' + firstname)
+} else if (firstname == "Pauline") {
+    // console.log('bonjour ma chère ' + firstname)
 } else {
-    console.log(`Mathieu n'a pas 30 ans`)
+    //Réécrire  le message loggé avec des backticks ``
+    // console.log('bonjour ' + firstname + ', je ne connaissais pas ton prénom avant')
+    // console.log(`Bonjour ${firstname}, je ne connaissais pas ton prénom avant`)
 }
+
+
+
+
+// VARIABLES COMPLEXES : LES TABLEAUX (Array)
+
+let arr1 = [3 ,5 ,0, 204.5, "hello", true, "world"] //On ommence par 0 et non 1
+
+// const firstElement = arr1[4]
+
+let arrResult1 = `${arr1[4]} ${arr1[6]}`
+
+
+let arrResult2 
+
+
+if("truthy") {
+    arrResult1 = `${arr1[4]} ${arr1[6]}` //hello world
+} else {
+    arrResult2 = arr1[1] + arr1[3] //209.5
+}
+
+
+// console.log(arrResult2)
+
+arr1[4] = "bonjour"
+
+
+
+arr1[3] -= 14.5
+// console.log(arr1)
+
+
+// VARIABLES COMPLEXES : LES OBJETS 
+
+let obj1 = {
+    maPremierePropriete : 12,
+    maDeuxiemePropriete : 14,
+    prop3 : 11,
+    prop9 : "hello"
+}
+
+// console.log(obj1.prop9)
+
+let identity = {
+    firstname: "Paul",
+    lastname: "Doazan",
+    age: 35,
+    town: "Talence"
+}
+
+// A l'aide de l'objet précédent, construire la chaine de caractère ==> 
+//  Je m'appelle Paul Doazan, j'ai 35 ans et j'habite Talence.
+
+let resultFromObject = `Je m'appelle ${identity.firstname} ${identity.lastname}, j'ai ${identity.age} ans et j'habite ${identity.town}.`
+
+console.log(resultFromObject)
+
+
+// Créer un deuxième objet identity2, stocker identity et identity2  dans un tableau, et écrire la phrase précédente en utilisant le tableau
+
+let identity2 = {
+    firstname: "Mathieu",
+    lastname: "Etourneaud",
+    age: 31,
+    town: "La Teste"
+}
+
+let arr2 = [identity, identity2]
+
+arr2[1].lastname
+
+// console.log(`Je m'appelle ${arr2[identity.firstname]} ${arr2[identity.lastname]}, j'ai ${arr2[identity.age]} ans et j'habite ${arr2[identity.town]}.`)
