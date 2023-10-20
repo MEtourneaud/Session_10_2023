eedb6b0799104f92d87b5d4141e3fa22927ab3fa
Mathieu Etourneaud <mathieu.etourneaud@lapiscine.pro>
2023-10-20 10:59:35 +0200

    Add correction of element in the middle of  array

--- cours/scripts/index_2.js
+++ cours/scripts/index_2.js
@@ -1,6 +1,6 @@
 // REVISIONS DE LA VEILLE
 
-let arrExample = [3, 5, 8, 12, 1]
+let arrExample = [3, 5, 8, 12, 1, 58, 23, 65, 35, 25, 1, 15, 33]
 
 let readFirstElement = true
 
@@ -18,13 +18,28 @@ let objectExample = {
     prop2 : 3
 }
 
-console.log(arrExample[arrExample.length -2])
+// console.log(arrExample[arrExample.length -2])
 
 
 // Récupérer l'élément du tableau placé au milieu du tableau à l'aide de la propriété .length du tableau
 
-if(arrExample%2) {
-    
+let middleIndex
+
+// console.log(arrExample.length)
+
+// let middleIndex = Math.round(arrExample.length / 2) ==> Méthode plus rapide
+
+if(arrExample.length%2 == 0) {
+    middleIndex = arrExample.length / 2
 } else {
-    
+    middleIndex = (arrExample.length - 1) /2
 }
+
+
+let elementInTheMiddle = arrExample[middleIndex]
+
+
+
+console.log(elementInTheMiddle)
+
+
