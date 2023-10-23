cd32a94036b24ce9b1fab2d9cd04f8f32eda822e
Mathieu Etourneaud <mathieu.etourneaud@lapiscine.pro>
2023-10-23 10:26:38 +0200

    Add AND OR in comparisons

--- cours/scripts/index_3.js
+++ cours/scripts/index_3.js
@@ -19,39 +19,52 @@
 const person_1 = {
     name : "Pierre",
     hasCar : true,
-    hasBike : true
+    hasBike : true,
+    isSick : true
 }
 
 const person_2 = {
     name : "Mélanie",
     hasCar : true,
-    hasBike : false
+    hasBike : false,
+    isSick : false
 }
 
 const person_3 = {
     name : "Paul",
     hasCar : false,
-    hasBike : true
+    hasBike : true,
+    isSick : true
 }
 
 const person_4 = {
     name : "Sophie",
     hasCar : false,
-    hasBike : false
+    hasBike : false,
+    isSick : false
 }
 
 
 
 //Intégrer le paramètre person aux 2 fonctions pour rendre la chaine de caractère dynamique
 
-canGoToUniversity(person_1.name)
-canGoToTopOfMountain(person_4.name)
+canGoToUniversity(person_2)
+canGoToTopOfMountain(person_1)
 
-function canGoToUniversity(person){
-    console.log(`${person} peut aller à l'université`)
-}
+// Opérateur de comparaison complexe : OR
+function canGoToUniversity(person) {
+    if(person.hasCar || person.hasBike && !person.isSick) {
+        console.log(`${person.name} peut aller à l'université`)
+    } else {
+        console.log(`${person.name} ne peut pas aller à l'université`)
+    }
+}  
 
-
-function canGoToTopOfMountain(person){
-    console.log(`${person} ne peut pas aller au sommet de la montagne`)
+// Opérateur de comparaison complexe : AND
+function canGoToTopOfMountain(person) {
+    if(person.hasCar && person.hasBike && !person.isSick) {
+        console.log(`${person.name} peut aller au sommet de la montagne`)
+    } else {
+        console.log(`${person.name} ne peut pas aller au sommet de la montagne`)
+    }   
 }
