2efef94dacc29304fe07dd7b3574150efe99e4c5
Mathieu Etourneaud <mathieu.etourneaud@lapiscine.pro>
2023-10-25 17:30:30 +0200

    Add pizzas data

--- exercices/algo_1/index.js
+++ exercices/algo_1/index.js
@@ -43,24 +43,142 @@ const nbs = [2, 12, 23, 5, 1, 8, 3, 104]
 
 // let moyenne
 
-const nbsAbove = getNumbersAboveAverage(nbs)
+// const nbsAbove = getNumbersAboveAverage(nbs)
 
-function getNumbersAboveAverage (arr) {
+// function getNumbersAboveAverage (arr) {
+    
+//     let sum = 0
+//     let moyenne 
+
+//     for (let n = 0; n < arr.length; n++) {
+//         moyenne = (sum += arr[n]) / arr.length
+//         // console.log(sum)
+//     }
+//     console.log(moyenne)
+    
+//     let compar = []
+//     for (let n = 1; n < arr.length; n++) {
+//         if (arr[n] > moyenne) {
+//             compar.push(arr[n])
+//         } 
+//     }
+//     console.log(compar)
+// }
+
+
+
+// Exercice des pizzas
+const pizzas = [
+    {
+        name: 'Margherita',
+        price: 11.50,
+        ingredients: ['mozzarella', 'tomate', 'basilic ', "huile d'olive"],
+        baseTomate: true
+    },
+    {
+        name: 'Regina',
+        price: 12,
+        ingredients: ['mozzarella', 'tomate', 'origan', 'jambon', 'champignons'],
+        baseTomate: true
+    },
+    {
+        name: '4 saisons',
+        price: 15,
+        ingredients: ['artichaut', 'courgette', 'poivron', 'mozzarella', 'oignon rouge'],
+        baseTomate: true
+    },
+    {
+        name: 'Napolitaine',
+        price: 14,
+        ingredients: ['tomate', 'mozzarella', 'anchois', 'olives noires', 'origan'],
+        baseTomate: true
+    },
+    {
+        name: '4 fromages',
+        price: 16,
+        ingredients: ['tomate', 'mozzarella', 'emmental', 'comté', 'roquefort'],
+        baseTomate: true
+    },
+    {
+        name: 'Montagnarde',
+        price: 19,
+        ingredients: ['mozzarella', 'reblochon', 'gruyère', 'oignon', 'champignon'],
+        baseTomate: false
+    },
+    {
+        name: 'Chèvre-miel',
+        price: 18,
+        ingredients: ['mozzarella', 'chèvre', 'miel'],
+        baseTomate: false
+    },
+    {
+        name: 'Hawaïenne',
+        price: 17,
+        ingredients: ['mozzarella', 'tomate', 'jambon', 'ananas'],
+        baseTomate: true
+    }
+]
+
+const pizzas2 = [
+    {
+        name: '8 fromages',
+        price: 10,
+        ingredients: ['mozzarella', 'reblochon', 'gruyère', 'oignon', 'champignon'],
+        baseTomate: false
+    },
+    {
+        name: '12 fromages',
+        price: 18,
+        ingredients: ['mozzarella', 'chèvre', 'miel'],
+        baseTomate: false
+    },
+]
+
+// Ecrire une fonction qui prend en paramètre un tableau de pizzas, et qui retourne un tableau de tous les noms des pizzas
+
+// const namePizza = getNamesPizza(pizzas)
+// console.log(namePizza)
+
+// function getNamesPizza(arr) {
+//     let pizzasArr = []
+
+//     for(let p = 0; p < arr.length; p++) {
+//         pizzasArr.push(arr[p].name)
+//     }
+//     return pizzasArr
+// }
+
+
+// Ecrire une fonction qui prend en paramètre un tableau de pizzas et qui me retourne la moyenne des prix
+
+const pizzasPriceAbove = getPriceAverage(pizzas)
+console.log(pizzasPriceAbove)
+
+function getPriceAverage(arr2) {
     
     let sum = 0
     let moyenne 
 
-    for (let n = 0; n < arr.length; n++) {
-        moyenne = (sum += arr[n]) / arr.length
-        // console.log(sum)
+    for (let n = 0; n < arr2.length; n++) {
+        moyenne = ((sum += arr2[n].price) / arr2.length).toFixed(2)
     }
-    console.log(moyenne)
-    
-    let compar = []
-    for (let n = 1; n < arr.length; n++) {
-        if (arr[n] > moyenne) {
-            compar.push(arr[n])
+    return moyenne
+}
+
+// Amélioration, la fonction renvoie un tableau de noms de pizzas dont le prix est supérieur à la moyenne
+
+const pizzasNamesAbove = getNamesAboveAverage(pizzas)
+console.log(pizzasNamesAbove)
+
+function getNamesAboveAverage(arr2) {
+
+    let moyenne = getPriceAverage(arr2)
+
+    let aboveAvg = []
+    for (let n = 1; n < arr2.length; n++) {
+        if (arr2[n].price > moyenne) {
+            aboveAvg.push(arr2[n].name)
         } 
     }
-    console.log(compar)
+    return aboveAvg
 }
