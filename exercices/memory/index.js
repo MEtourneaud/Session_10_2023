2fcc684d7ad033fa9e37bd02ec201bd91035ac1e
Mathieu Etourneaud <mathieu.etourneaud@lapiscine.pro>
2023-10-26 17:29:33 +0200

    Update memory game

--- exercices/memory/index.js
+++ exercices/memory/index.js
@@ -1,22 +1,32 @@
-const title = document.querySelector(`.title`)
-title.style.color = `white`
+const title = document.querySelector('.title')
+const cards = document.querySelectorAll('.card')
 
-const cards = document.querySelectorAll(`.card`)
-
-
-for(i = 0; i < cards.length; i++) {
+for (let i = 0; i < cards.length; i++) {
+    paramCard(cards[i], i)
     cards[i].addEventListener('click', onCardClick)
 }
 
 function onCardClick(event) {
     /**
-     * event.target est l'élément html sur lequel l'utilisateur a cliqué, event.currentTarget est l'élément html sur lequel addEventListener a été déclaré
+     * evet.target est l'élément html sur lequel l'utilisateur a cliqué, event.currentTarget est l'élément html sur lequel .addEventListener a été déclaré
      */
-    console.log(event.currentTarget)
+    const tg = event.currentTarget
+    if (tg.classList.contains('is-flipped')) {
+        tg.classList.remove('is-flipped')
+        setTimeout(cardBack, 3000)
+    } else {
+        tg.classList.add('is-flipped')
+    }
 }
 
+function cardBack() {
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].classList.add('is-flipped')
+    }
+}
 
-// var card = document.querySelector('.card');
-// card.addEventListener( 'click', function() {
-//   card.classList.toggle('is-flipped');
-// });
+function paramCard(card, index) {
+    card.style.order = (Math.random() * cards.length).toFixed()
+    card.children[0].children[0].textContent = ((index + 1) / 2).toFixed()
+    card.children[1].children[0].textContent = "?"
+}
