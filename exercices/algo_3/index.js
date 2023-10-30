986ebff94ddbd4fa24a569147e819562feeb4189
Mathieu Etourneaud <mathieu.etourneaud@lapiscine.pro>
2023-10-30 15:03:08 +0100

    map method

--- exercices/algo_3/index.js
+++ exercices/algo_3/index.js
@@ -4,16 +4,16 @@ import movies from './movies.json' assert {type: 'json'}
 
 // a. Logger le premier element du tableau movies
 
-console.log(movies[0])
+// console.log(movies[0])
 
 // b. Logger l'annee du 4eme element du tableau movies
 
-console.log(movies[3].year)
+// console.log(movies[3].year)
 
 
 // c. Logger le titre du dernier element du tableau movies
 
-console.log(movies[movies.length - 1].title)
+// console.log(movies[movies.length - 1].title)
 
 // d. Logger le titre du film qui a la meilleure note
 
@@ -24,7 +24,7 @@ movies.forEach((element) => {
         bestMovie = element
     }
 })
-console.log(bestMovie.title)
+// console.log(bestMovie.title)
 
 // e. Logger le titre du film le plus ancien
 
@@ -35,7 +35,7 @@ movies.forEach((element) => {
         oldestMovie = element
     }
 })
-console.log(oldestMovie.title)
+// console.log(oldestMovie.title)
 
 // f. Logger un tablea de tous les titres de film qui ont au minimun 3 acteurs
 
@@ -46,4 +46,59 @@ movies.forEach((element) => {
         movieTitles.push(element.title)
     }
 })
-console.log(movieTitles)
+// console.log(movieTitles)
+
+
+// 1. Ecrire une fonction qui prend en parametre le tableau movies et retourne un nouveau tableau de tous les titres de films
+
+
+const titleMovies = selectTitlesFromMovies(movies)
+// console.log(titleMovies)
+
+
+function selectTitlesFromMovies(arr) {
+    let selectedTitles = []
+
+    arr.forEach((element) => {
+        selectedTitles.push(element.title)
+    })
+    return selectedTitles
+}
+
+
+
+
+
+
+// 1a. Ecrire une fonction qui prend en parametre le tableau movies et retourne un nouveau tableau de tous les réalisateur de films
+
+
+// console.log(getAllDirectors(movies))
+
+// function getAllDirectors(arr) {
+//     let directors = []
+
+//     arr.forEach((element) => {
+//         directors.push(element.director)
+//     })
+//     return directors
+// }
+//-----------------------------------------------
+// function getAllDirectors(arr) {
+//     let directors = arr.map((element) => {
+//         return element.director
+//     })
+//     return directors
+// }
+//-----------------------------------------------
+function getAllDirectors(arr) {
+    return arr.map((element) => element.director)
+}
+
+
+// 1b.
+console.log(getAllYears(movies))
+
+function getAllYears(arr) {
+    return arr.map((element) => element.year)
+}
