2ab63f5af0e556e9a8fa07b950f4c5e9596717b0
Mathieu Etourneaud <mathieu.etourneaud@lapiscine.pro>
2023-10-30 11:04:06 +0100

    correct index_8.js

---
+++ cours/scripts/index_8.js
@@ -0,0 +1,52 @@
+/**
+ * addEventListener() // removeEventListener()
+ * 
+ * setTimeout(fonctionExecuteApresDelai, delai)
+ */
+
+const myTitle = document.querySelector('.my-title')
+
+const onClick = (event) => {
+    const tg = event.currentTarget
+    tg.classList.add('text-changed')
+
+    setTimeout(() => {
+        tg.classList.remove('text-changed')
+        tg.removeEventListener('click', onClick)
+    }, 2000)
+}
+
+const afficheEnConsole = () => {
+    console.log('hello world')
+}
+
+myTitle.addEventListener('click', onClick)
+
+
+/**
+* Exo sur les 3 boutons qui change la couleur du titre
+ */
+
+const btns = document.querySelectorAll('.btn')
+
+// for (let i = 0; i < btns.length; i++) {
+//     btns[i].addEventListener('click', (e) => {
+//         console.log(e.currentTarget)
+//     })
+// }
+
+btns.forEach((element) => {
+    element.addEventListener('click', (e) => {
+        myTitle.style.color = e.currentTarget.getAttribute('data-color')
+        myTitle.style.backgroundColor = e.currentTarget.getAttribute('data-bg-color')
+    })
+})
+
+
+//Ajouter un attribut data-background-color dans le .html, et l'utiliser en js pour changer le css
+
+// btns.forEach((element) => {
+//     element.addEventListener('click', (e) => {
+//         myTitle.style.backgroundColor = e.currentTarget.getAttribute('data-bg-color')
+//     })
+// })
